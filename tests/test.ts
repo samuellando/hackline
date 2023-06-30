2f3d2448e154114373b40748cda7155e251fd551
Samuel Lando <samuel.lando@aol.com>
2023-06-30 14:19:57 -0400

    Add progressive syncing abilities to the API client

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,9 @@ import { describe, expect, test } from '@jest/globals';
 import { ApiClient } from "$lib/Api";
 
 describe('sum module', () => {
+  let client = new ApiClient("", "")
   test('adds 1 + 2 to equal 3', () => {
+    console.log(client.getTimeline());
     expect(1 + 2).toBe(3);
   });
 });
