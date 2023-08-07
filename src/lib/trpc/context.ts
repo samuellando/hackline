d8d8eb4cbbc657a1260b7eac907fd53f0c21a4d7
Samuel Lando <samuel.lando@aol.com>
2023-08-07 12:05:08 -0400

    Refactor lib

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -7,11 +7,11 @@ export async function createContext(event: RequestEvent) {
 	let user;
 	if (!session?.user) {
 		const apiKey = event.request.headers.get('api-key');
-        if (!apiKey) {
-            user = null;
-        } else {
-            user = await getUser(apiKey);
-        }
+		if (!apiKey) {
+			user = null;
+		} else {
+			user = await getUser(apiKey);
+		}
 	} else {
 		user = session.user.id;
 	}
