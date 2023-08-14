375c3cd959ba71c9bd244b0b4da65c4fe74fc344
Samuel Lando <samuel.lando@aol.com>
2023-08-14 11:45:30 -0400

    Account page polish

--- src/routes/app/account/+page.svelte
+++ src/routes/app/account/+page.svelte
@@ -6,6 +6,7 @@
 	import { trpc } from '$lib/trpc/client';
 	import { page } from '$app/stores';
 	import Button from '$lib/components/Button.svelte';
+	import Nav from '$lib/components/Nav.svelte';
 	import { goto } from '$app/navigation';
 
 	export let data;
@@ -31,6 +32,9 @@
 	});
 
 	async function reset() {
+		if (!confirm('Are you sure? this will break any applications using this key!')) {
+			return;
+		}
 		let trpcClient = trpc($page);
 		await trpcClient.deleteApiKey.mutate();
 		apiKey = await trpcClient.getApiKey.query();
@@ -40,7 +44,7 @@
 
 	let errors: ContentErrors | null = null;
 
-	function save() {
+	async function save() {
 		let newSettings;
 		if (errors == null) {
 			if ('text' in content) {
@@ -49,7 +53,7 @@
 				newSettings = (content as JSONContent).json;
 			}
 
-			apiClient.setSettings(newSettings);
+			await apiClient.setSettings(newSettings);
 		}
 	}
 </script>
@@ -57,29 +61,63 @@
 <div class="flex flex-col min-h-screen">
 	<div class="basis-36" />
 	{#if !loading}
-		<p>Api Key: {apiKey}</p>
-		<Button onClick={reset} text="Reset" />
-		{#if stripeInfo.customerId && stripeInfo.paymentStatus == 'active'}
-			<form action="/app/payment?/dashboard" method="POST">
-				<button type="submit">Billing</button>
-			</form>
-		{:else}
-			<Button onClick={() => goto('/#pricing')} text="Subscribe" />
-		{/if}
-		<div>
-			<JSONEditor
-				bind:content
-				mode={Mode.text}
-				onChange={(_b, _a, { contentErrors }) => {
-					errors = contentErrors;
-				}}
-			/>
+		<p class="text-5xl pb-5 text-center">Settings</p>
+		<hr class="pb-5" />
+		<div class="flex flex-col items-center">
+			{#if errors == null}
+				<div class="px-5 pb-5">
+					<Button onClick={save} text="Save" />
+				</div>
+			{:else}
+				<div class="px-5 pb-5">
+					<Button
+						onClick={() => {
+							alert("Can't save with errors");
+						}}
+						text="Save"
+					/>
+				</div>
+			{/if}
+			<div class="my-json-editor lg:w-1/2">
+				<JSONEditor
+					bind:content
+					mode={Mode.text}
+					onChange={(_b, _a, { contentErrors }) => {
+						errors = contentErrors;
+					}}
+				/>
+			</div>
 		</div>
 
-		{#if errors == null}
-			<div class="pt-5">
-				<Button onClick={save} text="Save" />
-			</div>
-		{/if}
+		<p class="text-5xl py-5 text-center">Billing</p>
+		<hr class="pb-5" />
+		<div class="flex flex-col items-center">
+			{#if stripeInfo.customerId && stripeInfo.paymentStatus == 'active'}
+				<form action="/app/payment?/dashboard" method="POST">
+					<button type="submit">Billing</button>
+				</form>
+			{:else}
+				<Button onClick={() => goto('/#pricing')} text="Subscribe" />
+			{/if}
+		</div>
+
+		<p class="text-5xl py-5 text-center">API key</p>
+		<hr class="pb-5" />
+		<div class="flex flex-col items-center h-screen gap-2 px-5">
+			<p class="text-xl">
+				This is your API key. It is used to authenticate your requests to the API, it will only show
+				once, so be sure to copy it!
+			</p>
+			<p class="text-2xl">Api Key: {apiKey}</p>
+			<Button onClick={reset} text="Reset" />
+		</div>
 	{/if}
 </div>
+
+<style>
+	.my-json-editor {
+		/* define a custom theme color */
+		--jse-theme-color: var(--primary);
+		--jse-theme-color-highlight: var(--secondary);
+	}
+</style>
