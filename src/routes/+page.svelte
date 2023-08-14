332633655da885e35e99a959394d6faeb09509f4
Samuel Lando <samuel.lando@aol.com>
2023-08-14 13:34:18 -0400

    Finalize home page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -114,15 +114,46 @@
 
 <div id="readmore" class="flex flex-col min-h-screen">
 	<div class="basis-36" />
+	<p class="text-center text-5xl">Principles</p>
+	<div class="flex flex-col lg:flex-row gap-10 justify-center">
+		<div class="w-full lg:w-1/2">
+			<p class="px-5 pt-10 text-lg">
+				Hackline is a <a href="https://github.com/samuellando/hackline" class="underline">
+					open source
+				</a> time tracking tool designed for personal time tracking.
+			</p>
+			<p class="px-5 pt-5 text-lg">
+				<b>It will always remain open source</b>. You are free to deploy your own instance, or you
+				can use the hosted version, and support the project.
+			</p>
+			<p class="px-5 pt-5 text-lg">
+				Hackline is "opinionated" in the sense that it is designed for personal time tracking to
+				help you measure how productive you really are. It is not designed to be used for team time
+				tracking, or for billing clients, but for independent professionals and students who want to
+				track their time.
+			</p>
+			<p class="px-5 pt-5 text-lg">There are a few principles that guide the design of Hackline:</p>
+			<ol class="px-10 py-5 list-decimal text-lg">
+				<li>
+					Follow the <a href="https://en.wikipedia.org/wiki/Unix_philosophy" class="underline"
+						>Uinx Philosophy</a
+					>
+					<ol class="list-disc list-inside">
+						<li>Do one thing and do it well</li>
+						<li>Work together with other programs</li>
+					</ol>
+				</li>
+				<li>you do one thing at a time, so it tracks one thing at a time</li>
+				<li>you are not a robot, tracking should be as automated as possible.</li>
+				<li>You should be able to quickly build around Hackline and add features that you need.</li>
+			</ol>
+		</div>
+	</div>
 	<p class="text-center text-5xl">How it works</p>
-	<p class="p-10 text-lg text-center">
-		Hackline is a <a href="https://github.com/samuellando/hackline" class="underline">
-			open source
-		</a> time tracking tool designed for personal time tracking. It has some cheap hosted options below.
-	</p>
-	<div class="flex justify-center flex-col lg:flex-row gap-5">
+	<p class="text-center text-3xl pb-5">Track one thing at a time</p>
+	<div class="px-5 flex justify-center items-center flex-col lg:flex-row gap-5">
 		<div>
-			<p class="text-lg w-screen lg:w-1/2 pb-5">
+			<p class="text-lg w-full lg:w-1/2 pb-5">
 				It tracks your time in non overlapping intervals of time, and displays everything in a nice
 				timeline view.
 			</p>
@@ -140,19 +171,32 @@
 			<img src="/after.png" alt="after" />
 		</p>
 	</div>
-	<p class="p-10" />
+
+	<p class="text-center text-3xl pb-5">Fully customizable</p>
+	<div class="px-5 flex justify-center items-center flex-col lg:flex-row gap-5">
+		<div class="">
+			<img src="/settings.png" alt="settings page screenshot" />
+		</div>
+		<div class="w-full lg:w-1/4 pb-5">
+			<p class="text-xl pb-5">
+				The web interface is designed to be fully customizable! Almost everything is a parameter
+				that can be changed in the settings.
+			</p>
+		</div>
+	</div>
+
 	<div id="pricing" class="basis-36" />
 	<p class="text-center text-5xl">Pricing</p>
 	{#if data.session?.user && data.stripeInfo.paymentStatus == 'inactive'}
-		<p class="text-center">You must support in order to maintain access.</p>
+		<p class="text-xl text-center">You must support in order to maintain access.</p>
 	{/if}
 	{#if data.session?.user && data.stripeInfo.paymentStatus == 'trial'}
-		<p class="text-center">You are in the 7 day free trial.</p>
+		<p class="text-xl text-center">You are in the 7 day free trial.</p>
 	{/if}
-	<p class="text-center py-10">
-		After the 7 day free trial, there are currently two ways to get access
+	<p class="text-xl text-center py-10">
+		After the 7 day free trial, there are currently two ways to maintain access
 	</p>
-	<div class="flex flex-row gap-2 lg:gap-20 justify-center">
+	<div class="text-md lg:text-lg flex flex-row gap-2 lg:gap-20 justify-center">
 		<div class="border w-2/5 lg:w-1/4 border-[var(--secondary)] text-center p-5">
 			<p class="text-lg lg:text-2xl">Early Adopter</p>
 			<hr />
@@ -169,9 +213,10 @@
 		<div class="border w-2/5 lg:w-1/4 border-[var(--secondary)] text-center p-5">
 			<p class="text-center text-lg lg:text-2xl">Subscription</p>
 			<hr />
-			<p><b>$1 a month forever</b></p>
+			<p><b>$1 a month</b></p>
 			<p>Limited time offer</p>
 			<p>Access to all future features</p>
+			<p>Cancel any time</p>
 			{#if data.session?.user && data.stripeInfo.paymentStatus != 'active'}
 				<form action="/payment?/subscribe" method="POST">
 					<button type="submit" class="underline">Subscribe</button>
@@ -180,13 +225,20 @@
 		</div>
 	</div>
 	{#if data.session?.user && data.stripeInfo.paymentStatus == 'active'}
-		<p class="pt-10 text-center">You are already subscribed, thank you for your support!</p>
+		<p class="text-lg pt-10 text-center">You are already subscribed, thank you for your support!</p>
 		<div class="w-screen flex justify-center">
 			<Nav text="Manage Subscription" onClick={() => goto('/app/account')} />
 		</div>
 	{:else}
-		<p class="pt-10 text-center">All payments are handled by stripe, and are secure.</p>
+		<p class="text-lg pt-10 text-center">All payments are handled by stripe, and are secure.</p>
 	{/if}
+	<p class="text-lg pt-10 text-center">
+		If you open a accepted issue or feature request on the <a
+			href="https://github.com/samuellando/hackline/issues"
+			class="underline">github</a
+		> I will reach out to you and give you a free access forever.
+	</p>
+
 	<div id="docs" class="basis-36" />
 	<p class="text-center text-5xl">Docs</p>
 	<p class="text-center text-2xl"><a class="underline" href="#top">API Reference</a></p>
